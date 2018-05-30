62cf9235fb09059cf0530c615414b3099c885ff8
gizyk <gizyk9@gmail.com>
2018-05-30 13:09:43 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var webpack = require('webpack'),
+    path = require('path');
+ 
+var srcPath  = path.join(__dirname, '/work/js'),
+    distPath = path.join(__dirname, '/dist/js');
+ 
+module.exports = {
+    cache: true,
+    devtool: '#cheap-module-eval-source-map',
+    context: srcPath,
+    entry: {
+        main: './main.js',
+    },
+    output: {
+        path: distPath,
+        filename: '[name].bundle.js',
+    },
+    resolve: {
+        modules: ["node_modules"]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
+    ]
+};
